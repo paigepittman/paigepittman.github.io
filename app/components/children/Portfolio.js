fb1f5bdd3b71c2b6a40268f4a93c332124704798
Paige Pittman <paigepittman@Paiges-Air.att.net>
2017-07-02 12:27:21 -0700

    links

--- app/components/children/Portfolio.js
+++ app/components/children/Portfolio.js
@@ -18,7 +18,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Capitol Hill</h5>
-                      <a href="https://capitol-hill.herokuapp.com">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://capitol-hill.herokuapp.com" target="_blank">VIEW SITE</a> | <a href="https://github.com/Capitol-Hill/Capitol-Hill" target="_blank">VIEW CODE</a>
                       <p>Capitol Hill is an application designed to easily connect people with their Senators and Congressional representatives, and to keep constituents up to date with their representative’s legislative actions.</p><br/><br/>
                       Role: Front-end Developer<br/>
 
@@ -34,7 +34,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">GroopUp</h5>
-                      <a href="http://groopup.herokuapp.com/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="http://groopup.herokuapp.com/" target="_blank">VIEW SITE</a> | <a href="https://github.com/GroopUp/GroopUp" target="_blank">VIEW CODE</a>
                       <p>EventBrite meets Groupon. "GroopUp" offers discounted rates on activities with the opportunity to meet people with similar interest. Take a personality test to find the most compatible events for you.</p><br/><br/>
                       Role: Front-end Developer<br/>
                     </div>
@@ -50,7 +50,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Music++</h5>
-                      <a href="https://kikanga.github.io/Artist-Direct/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://kikanga.github.io/Artist-Direct/" target="_blank">VIEW SITE</a> | <a href="https://github.com/kikanga/Artist-Direct" target="_blank">VIEW CODE</a>
                       <p>Music++ is creates a one-stop shop for music lovers' needs. One quick search transports the user to a music hub; listen to their top songs, buy tickets to their upcoming shows, read their tweets and more.</p><br/>
                       Role: Front-end Developer<br/>
                     </div>
@@ -69,7 +69,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Burger Buddy</h5>
-                      <a href="https://safe-badlands-36347.herokuapp.com/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://safe-badlands-36347.herokuapp.com/" target="_blank">VIEW SITE</a> | <a href="https://github.com/paigepittman/sequel-burger" target="_blank">VIEW CODE</a>
 
                       <p>Burger Buddy allows the user to 'eat', 'order' and 'build your own burger' through sequelize and a sql database</p><br/><br/>
                       Role: Sole Developer<br/>
@@ -87,7 +87,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Crystal Collector</h5>
-                      <a href="https://paigepittman.github.io/week-4-game/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://paigepittman.github.io/week-4-game/" target="_blank">VIEW SITE</a> | <a href="https://github.com/paigepittman/week-4-game" target="_blank">VIEW CODE</a>
                       <p>Crystal Collector allows the user to pick crystals with random values to try to get a pre-determined score without going over</p><br/><br/>
                       Role: Sole Developer<br/>
                     </div>
@@ -103,7 +103,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Harry Potter Trivia</h5>
-                      <a href="https://paigepittman.github.io/TriviaGame/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://paigepittman.github.io/TriviaGame/" target="_blank">VIEW SITE</a> | <a href="https://github.com/paigepittman/TriviaGame" target="_blank">VIEW CODE</a>
 
                       <p>JavaScript based trivia game
                       </p><br/><br/><br/><br/>
@@ -124,7 +124,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">giftastic</h5>
-                      <a href="https://paigepittman.github.io/giftastic/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://paigepittman.github.io/giftastic/" target="_blank">VIEW SITE</a> | <a href="https://github.com/paigepittman/giftastic" target="_blank">VIEW CODE</a>
 
                       <p>JavaScript application using the Giphy API to generate random gifs and create custom buttons for gif categories.</p><br/><br/><br/>
                       Role: Sole Developer<br/>
@@ -142,7 +142,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Train Times</h5>
-                      <a href="https://paigepittman.github.io/train-times/">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times">VIEW CODE</a>
+                      <a href="https://paigepittman.github.io/train-times/" target="_blank">VIEW SITE</a> | <a href="https://github.com/paigepittman/train-times" target="_blank">VIEW CODE</a>
 
                       <p>JavaScript application using a firebase database to store train times. Users can add new trains and their next arrivals will be automatically calcuated for the user.</p><br/><br/>
                       Role: Sole Developer<br/>
@@ -159,7 +159,7 @@ var Portfolio = React.createClass({
                     </div>
                     <div className="card-content">
                       <h5 className="title">Flashcard Generator</h5>
-                      <a href="https://github.com/paigepittman/flashcard-generator">VIEW CODE</a>
+                      <a href="https://github.com/paigepittman/flashcard-generator" target="_blank">VIEW CODE</a>
 
                       <p>Node application for creating flashcards with the option to use fill in the blank cards or basic.</p><br/><br/><br/>
                       Role: Sole Developer<br/>
